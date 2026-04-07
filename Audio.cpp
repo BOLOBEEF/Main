af55866949b039d007a620ffc76a1e751add3155
Ali El_Shaer <shaer1932008@gmail.com>
2026-04-07 23:17:56 +0200

    added sounds

--- Audio.cpp
+++ Audio.cpp
@@ -9,6 +9,8 @@
 
 // Structs and enums
 
+
+
 enum MenuSoundEffect
 {
 	// add a type for each menu sound effect
@@ -43,15 +45,17 @@ SoundBuffer menuMusicSound;
 SoundBuffer gameMusic_SlowSound;
 SoundBuffer gameMusic_FastSound;
 SoundBuffer gameMusic_DarkSound;
-SoundBuffer StepsSound;
-SoundBuffer PondStepsSound;
+
+SoundBuffer StepsSound;//->
+SoundBuffer IceSteps_WgSound;//->
+SoundBuffer IceSteps_FbSound;//->
+SoundBuffer PondStepsSound;//->
+SoundBuffer DeathSound;//->
+SoundBuffer BoyJumpSound;//->
+SoundBuffer GirlJumpSound;//->
+
 SoundBuffer PondFreezeSound;
 SoundBuffer PondMeltSound;
-SoundBuffer IceSteps_WgSound;
-SoundBuffer IceSteps_FbSound;
-SoundBuffer DeathSound;
-SoundBuffer BoyJumpSound;
-SoundBuffer GirlJumpSound;
 SoundBuffer WindSound;
 SoundBuffer DoorSound;
 SoundBuffer ButtonSound;
@@ -65,8 +69,23 @@ SoundBuffer DiamondCollectingSound;
 
 // have only one Music player and 5 sound effect players
 Sound musicPlayer;
+
 int currentSoundEffectPlayer = 0;
+
 Sound soundEffectsPlayers[5];
+
+Sound PondFreeze;
+Sound PondMelt;
+Sound Wind;
+Sound Door;
+Sound Button; //
+Sound Lever;
+Sound Platform;
+Sound LevelWin;//
+Sound LevelGameover;
+Sound Clock;
+Sound DiamondCollecting;
+
 // loop on those sound effect players when playing sound effects, so we can play multiple sound effects at the same time without cutting each other off, but we can only play 5 sound effects at the same time
 
 
@@ -75,18 +94,21 @@ Sound soundEffectsPlayers[5];
 void InitializeAudio()
 {
 	menuMusicSound.loadFromFile("/Main/Assets/UsedSounds/MenuMusic.mp3");
+
 	gameMusic_SlowSound.loadFromFile("/Main/Assets/UsedSounds/LevelMusic_slow.mp3");
 	gameMusic_FastSound.loadFromFile("/Main/Assets/UsedSounds/LevelMusic_fast.mp3");
 	gameMusic_DarkSound.loadFromFile("/Main/Assets/UsedSounds/LevelMusic_dark.mp3");
+
 	StepsSound.loadFromFile("/Main/Assets/UsedSounds/Steps.mp3");
 	PondStepsSound.loadFromFile("/Main/Assets/UsedSounds/PondSteps.mp3");
-	PondFreezeSound.loadFromFile("/Main/Assets/UsedSounds/PondFreeze.mp3");
-	PondMeltSound.loadFromFile("/Main/Assets/UsedSounds/PondMelt.mp3");
-	IceSteps_WgSound.loadFromFile("/Main/Assets/UsedSounds/IceSteps_wg.mp3");//??
-
+	IceSteps_WgSound.loadFromFile("/Main/Assets/UsedSounds/IceSteps_wg.mp3");
+	IceSteps_FbSound.loadFromFile("/Main/Assets/UsedSounds/IceSteps_fb.mp3");
 	DeathSound.loadFromFile("/Main/Assets/UsedSounds/Death.mp3");
 	BoyJumpSound.loadFromFile("/Main/Assets/UsedSounds/Jump_fb.mp3");
 	GirlJumpSound.loadFromFile("/Main/Assets/UsedSounds/Jump_wg.mp3");
+
+	PondFreezeSound.loadFromFile("/Main/Assets/UsedSounds/PondFreeze.mp3");
+	PondMeltSound.loadFromFile("/Main/Assets/UsedSounds/PondMelt.mp3");
 	WindSound.loadFromFile("/Main/Assets/UsedSounds/Wind.mp3");
 	DoorSound.loadFromFile("/Main/Assets/UsedSounds/Door.mp3");
 	ButtonSound.loadFromFile("/Main/Assets/UsedSounds/Button.mp3");
@@ -106,19 +128,24 @@ void PlayMusic(BackgroundMusic music) {
 	{
 	case MainMenu:
 		musicPlayer.setBuffer(menuMusicSound);
+		musicPlayer.setVolume(100.0f);
 		break;
 	case Game_Slow:
+		musicPlayer.setBuffer(gameMusic_SlowSound);
+		musicPlayer.setVolume(100.0f);
 		break;
 	case Game_Fast:
+		musicPlayer.setBuffer(gameMusic_FastSound);
+		musicPlayer.setVolume(100.0f);
 		break;
 	case Game_Dark:
+		musicPlayer.setBuffer(gameMusic_DarkSound);
+		musicPlayer.setVolume(100.0f);
 		break;
 	default:
 		cout << "Invalid background music" << endl;
 		validMusic = false;
 		break;
-
-
 	}
 
 
@@ -133,12 +160,16 @@ void PlayMenuSoundEffect(MenuSoundEffect soundEffect) {
 	switch (soundEffect)
 	{
 	case ButtonClick:
-		break;
-	case ButtonSwitch:
+		Button.setBuffer(ButtonSound);
+		Button.setVolume(100.0f);
 		break;
 	case Back:
+		Button.setBuffer(ButtonSound);
+		Button.setVolume(100.0f);
 		break;
 	case Enter:
+		Button.setBuffer(ButtonSound);
+		Button.setVolume(100.0f);
 		break;
 	default:
 		cout << "Invalid sound effect" << endl;
@@ -152,15 +183,23 @@ void PlayGameSoundEffect(GameSoundEffect soundEffect) {
 	switch (soundEffect)
 	{
 	case Death:
+
 		break;
 	case DoorOpen:
+		Door.setBuffer(DoorSound);
+		Door.setVolume(100.0f);
 		break;
 	case DoorClose:
+		Door.setBuffer(DoorSound);
+		Door.setVolume(100.0f);
 		break;
 	case Win:
+		LevelWin.setBuffer(LevelWinSound);
+		LevelWin.setVolume(100.0f);
 		break;
 	default:
 		cout << "Invalid sound effect" << endl;
 		break;
 	}
 }
+
