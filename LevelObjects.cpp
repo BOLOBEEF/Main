304ae3e9c5c1b4bbd8cc7142d63f52fc0d4c0c7e
Null <stephanoashraf64@gmail.com>
2026-04-07 10:08:48 +0200

    Revert "شوية حاجات لبركة السبع"
    This reverts commit 2f296aa0836e44d94b190322532c005409b02db4.

--- LevelObjects.cpp
+++ LevelObjects.cpp
@@ -1,13 +1,12 @@
 
 // STRUCTS for level objects
-
-//struct for the player
 struct Player
 {
 	RectangleShape sprite = RectangleShape(Vector2f(100, 100));
 	Vector2f velocity = { 0,0 };
 	const float speed = 100.0f;
 	const float gravity = 200.0f;
+
 	Player() {
 
 		sprite.setOrigin(sprite.getLocalBounds().width / 2.0f, sprite.getLocalBounds().height / 2.0f);
@@ -27,57 +26,5 @@ struct Player
 
 		sprite.move(velocity * dt);
 
-	}
-};
-
-//struct for ponds
-struct Ponds
-{
-	RectangleShape ponds_sprite = RectangleShape(Vector2f(150, 100));
-	Ponds() {
-		ponds_sprite.setOrigin(ponds_sprite.getLocalBounds().width / 2.0f, ponds_sprite.getLocalBounds().height / 2.0f);
-		ponds_sprite.setPosition(center.x+200, center.y + 200);
-	}
-	void PondsEffect(Player& player,Which_player who,pond_state pond) {
-		// 
-		switch (pond)
-		{
-			case POISON_POND:
-				//statment
-				switch (who)
-				{
-				case FIRE_PLAYER:
-					if()
-					break;
-				case WATER_PLAYER:
-
-					break;
-				}
-				break;
-			case FIRE_POND:
-				//statment
-				switch (who)
-				{
-				case FIRE_PLAYER:
-
-					break;
-				case WATER_PLAYER:
-
-					break;
-				}
-				break;
-			case WATER_POND:
-				//statment
-				switch (who)
-				{
-				case FIRE_PLAYER:
-
-					break;
-				case WATER_PLAYER:
-
-					break;
-				}
-				break;
-		}
 	}
 };
