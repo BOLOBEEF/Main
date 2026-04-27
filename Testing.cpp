022a3fcae585619e017856355a2fd1aae402cda0
AhmedManie <am0090414@gmail.com>
2026-04-27 21:00:07 +0300

    Update Testing.cpp

--- Testing.cpp
+++ Testing.cpp
@@ -1,6 +1,6 @@
 
 // TESTING
-FinalDoor door = FinalDoor(FinalDoor::FIRE_DOOR, Vector2f(1200, 25));
+
 // this is a script just for testing out functions or logic
 // when you add a new function and u want to test it, you can call the function from here
 
@@ -8,7 +8,7 @@ void InitializeTest() {
 	// for example test if a sound function is working here
 	// PlayMusic(BackgroundMusic::MainMenu);
 	// test push
-	ApplyTexture(door.sprite, LoadTexture::fire_door_open_texture, Vector2f(80, 80));
+	
 }
 
 void HandleTestInput(Event event)
@@ -24,7 +24,8 @@ void OnUpdatedGameStateTest() {
 
 void UpdateTest()
 {
-	DoorUpdateAnimation(door);
+	
+
 	
 }
 
@@ -32,6 +33,6 @@ void DrawTest() {
 	
 	// for example test if a sound function is working here
 	// PlayMusic(BackgroundMusic::MainMenu);
-	window.draw(door.sprite);
+	
 	
 }
